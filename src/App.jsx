307e230975cefeee51acba51976de063d4aa78a0
khjoo1203 <khjoo1203@naver.com>
2022-08-19 16:28:16 +0900

    투두리스트 2 초안

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoListContainer from "./components/TodoListContainer";
+import styled from "styled-components";
+import AddForm from "./components/AddForm";
+
+function App() {
+  return (
+    <StContainer>
+      <AddForm />
+      <TodoListContainer />
+    </StContainer>  
+  );
+}
+
+export default App;
+
+const StContainer = styled.section`
+max-width: 1440px;
+margin: 0 auto;
+`;
